34d7365f0af081d0653615e4377b5b0e30956105
jmcfee <jmcfee@umich.edu>
2023-01-24 16:20:40 -0500

    added rectangle class and all member functions

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -6,7 +6,9 @@ class Rectangle():
     # It sets an instance variable, "height" to the passed argument, height
 
     # YOUR CODE HERE
-
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
     # Create the "__str__" method
@@ -15,7 +17,8 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
-
+    def __str__(self):
+        return f'A rectangle with width {self.width} and height {self.height}'
 
 
     # Create the "verify_input" method
@@ -25,7 +28,10 @@ class Rectangle():
     #       False otherwise
 
     # YOUR CODE HERE
-
+    def verify_input(self):
+        if (self.width > 0 and self.height > 0):
+            return True
+        return False
 
 
     # Create the "area" method
@@ -34,7 +40,10 @@ class Rectangle():
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
-
+    def area(self):
+        if (not self.verify_input()):
+            return "Invalid input"
+        return self.width*self.height
 
 
     # Create the "perimeter" method
@@ -43,7 +52,10 @@ class Rectangle():
     # Otherwise, it returns the perimeter of the rectangle.
 
     # YOUR CODE HERE
-    
+    def perimeter(self):
+        if (not self.verify_input()):
+            return "Invalid input"
+        return (2*self.width) + (2*self.height)
 
 
 def main():
